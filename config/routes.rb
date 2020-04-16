803fb47aab3ecebb8419da3fd76db2ce05626f79
yuki takagi <yuki99takagi@gmail.com>
2020-04-16 14:38:03 +0900

    コメント機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'sessions#new'
-  resources :blogs
+  resources :blogs do
+    resources :comments
+  end
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
 end
